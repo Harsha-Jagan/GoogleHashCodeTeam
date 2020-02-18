93c26849a4e294a8baa524824a8f8dc4f6059c7a
Pranav Rayudu <tarakapranav@gmail.com>
2020-02-18 14:06:16 -0600

    formatted Example.java and added problem statement

--- Example.java
+++ Example.java
@@ -1,53 +1,64 @@
-import java.util.*;
-import java.io.*;
-public class Example{
-  private static ArrayList<Integer> list = new ArrayList<>();
-  public static void main(String[] args) throws IOException{
-    //get input
-    Scanner scan = new Scanner(new File("C:\\Users\\harsh\\OneDrive\\Desktop\\GoogleHashCode\\inputData\\b_small.in"));
-    int maxCap = scan.nextInt();
-    int numSlices = scan.nextInt();
-    int[] slices = new int[numSlices];
-    for(int a=0; a<numSlices; a++){
-      slices[a] = scan.nextInt();
-    }
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Example {
+    
+    private static ArrayList<Integer> list = new ArrayList<>();
+    
+    public static void main(String[] args) throws IOException {
+        //get input
+        Scanner scan = new Scanner(new File("inputPath\\b_small.in"));
+        int maxCap = scan.nextInt();
+        int numSlices = scan.nextInt();
+        int[] slices = new int[numSlices];
+        for (int a = 0; a < numSlices; a++) {
+            slices[a] = scan.nextInt();
+        }
 //testing
 //    int out = helper(slices, 0, maxCap);
-    int out = helperDP(slices, maxCap);
-    //write output
-    FileWriter writer = new FileWriter("C:\\Users\\harsh\\OneDrive\\Desktop\\GoogleHashCode\\output\\b.out");
-    StringBuilder output = new StringBuilder();
-    output.append(list.size());
-    output.append("\n");
-    for(int a=0; a<list.size(); a++){
-      output.append(list.get(a));
-      output.append(" ");
+        int out = helperDP(slices, maxCap);
+        //write output
+        FileWriter writer = new FileWriter("./output\\b.out");
+        StringBuilder output = new StringBuilder();
+        output.append(list.size());
+        output.append("\n");
+        for (int a = 0; a < list.size(); a++) {
+            output.append(list.get(a));
+            output.append(" ");
+        }
+        writer.write(output.toString());
+        writer.close();
     }
-    writer.write(output.toString());
-    writer.close();
-  }
-
-  private static int helper(int[] slices, int idx, int cap){
-    if(idx == slices.length){
-      return 0;
+    
+    private static int helper(int[] slices, int idx, int cap) {
+        
+        if (idx == slices.length) {
+            return 0;
+        }
+        
+        int with = 0;
+        if (cap - slices[idx] > 0) {
+            with = slices[idx] + helper(slices, idx + 1, cap - slices[idx]);
+        }
+        int without = helper(slices, idx, cap);
+        
+        if (with > without) {
+            if (!list.contains(idx)) {
+                list.add(idx + 1);
+            }
+            return with;
+        }
+        
+        return without;
     }
-
-    int with = 0;
-    if(cap - slices[idx] > 0)
-      with = slices[idx] + helper(slices, idx + 1, cap - slices[idx]);
-    int without = helper(slices, idx, cap);
-
-    if(with > without){
-      if(!list.contains(idx))
-        list.add(idx+1);
-      return with;
+    
+    private static int helperDP(int[] slices, int maxCap) {
+        
+        int[][] temp = new int[slices.length][maxCap];
+//        for (int a = 0; a <)
+        return 0;
     }
-
-    return without;
-  }
-
-  private static int helperDP(int[] slices, int maxCap){
-    int[][] temp = new int[slices.length][maxCap];
-    for(int a=0; a<)
-  }
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 Repo for the Google Hash Code Project 2020.
 
-[Click here](https://codingcompetitions.withgoogle.com/hashcode) for more info.
+[Click here](https://codingcompetitionpus.withgoogle.com/hashcode) for more info.
